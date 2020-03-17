9d3bd8176e07933de15c9d8a3a0dcb65b327db97
Julius Schulz <58485703+Julius1812@users.noreply.github.com>
2020-03-17 14:21:54 +0100

    first commit

---
+++ wine_model.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_wine
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+def train_model(X, y)
+    m = LinearRegression()
+    m.fit(df, y)
+    return m 
+
+
+
+
+if __name__ == '__main__':
+    d = load_wine()
+    df = pd.DataFrame(d['data'], columns=d['feature_names'])
+    y = d['target']  # cultivator
+
+
+    m.score(df, y)
